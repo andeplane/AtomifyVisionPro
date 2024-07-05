a8f0f6b84564f6c42c5de5e3215dfc6b79088e5a
Anders Hafreager <andershaf@gmail.com>
2024-07-05 19:42:14 +0200

    Added simulations

---
+++ Atomify/EnvironmentKeys.swift
@@ -0,0 +1,8 @@
+//
+//  EnvironmentKeys.swift
+//  Atomify
+//
+//  Created by Anders Hafreager on 05/07/2024.
+//
+
+import Foundation
---
+++ Atomify/SimulationCard.swift
@@ -0,0 +1,18 @@
+//
+//  SimulationCard.swift
+//  Atomify
+//
+//  Created by Anders Hafreager on 05/07/2024.
+//
+
+import SwiftUI
+
+struct SimulationCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimulationCard()
+}
